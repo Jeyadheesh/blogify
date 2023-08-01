48e47735a83078f3f0ce0a4166db81c80b1dd8b1
Jeyadheesh <jeyadheesh.nagarajan@gmail.com>
2023-08-01 14:33:10 +0530

    change img

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3306;
+const port = 9000;
 const dotenv = require("dotenv");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
